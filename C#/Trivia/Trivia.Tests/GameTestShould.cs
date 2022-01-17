965cbe8d2423d2a8035c4124d1daea48b6131bc6
Oscar Herrero <oherrero@aidacanarias.com>
2022-01-17 15:19:36 +0100

    ✅ verify_output_for_three_players_for_1000_games

--- C#/Trivia/Trivia.Tests/GameTestShould.cs
+++ C#/Trivia/Trivia.Tests/GameTestShould.cs
@@ -7,20 +7,34 @@ namespace Trivia.Tests
 {
     public class GameTestShould
     {
-        private const string folder = @"../../../Output";
+        private const string Folder = @"../../../Output";
 
-        [Fact]
+        [Fact(Skip = "true")]
         public void create_golden_master_files()
         {
-            Directory.CreateDirectory(folder);
+            Directory.CreateDirectory(Folder);
 
             for (var i = 1; i <= 1000; i++)
             {
                 var file = $"trivialPlay.{i}.txt";
-                using var fileStream = new FileStream(Path.Combine(folder, file), FileMode.Create);
+                using var fileStream = new FileStream(Path.Combine(Folder, file), FileMode.Create);
                 using var outputStream = new StreamWriter(fileStream);
                 Console.SetOut(outputStream);
                 GameRunner.PlayGame(new Random(i));
+            }
+        }
+
+        [Fact]
+        public void verify_output_for_three_players_for_1000_games()
+        {
+            for (var i = 1; i <= 1000; i++)
+            {
+                var file = $"trivialPlay.{i}.txt";
+                var expectedContent = new StringBuilder();
+                Console.SetOut(new StringWriter(expectedContent));
+
+                GameRunner.PlayGame(new Random(i));
+                Assert.Equal(expectedContent.ToString(), File.ReadAllText(Path.Combine(Folder, file)));
             }
         }
     }
